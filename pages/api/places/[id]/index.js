5ef578ada8845826f2e8c9d9219fcb7694f75057
Lueder Budde <lueder.budde@gmail.com>
2023-11-08 10:02:13 +0100

    add try and catch to api routes

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -7,26 +7,42 @@ export default async function handler(request, response) {
 
   // GET request:
   if (request.method === "GET") {
-    const place = await Place.findById(id);
-    if (!place) {
-      return response.status(404).json({ status: "Not Found" });
+    try {
+      const place = await Place.findById(id);
+      if (!place) {
+        return response.status(404).json({ status: "Not Found" });
+      }
+      response.status(200).json(place);
+    } catch (error) {
+      response.status(500).json({ status: "Internal Server Error" });
     }
-    response.status(200).json(place);
   }
+
   // PATCH request:
   if (request.method === "PATCH") {
-    const place = await Place.findByIdAndUpdate(id, {
-      $set: request.body,
-    });
-    if (!place) {
-      return response.status(404).json({ status: "Not Found" });
-    }
+    try {
+      const place = await Place.findByIdAndUpdate(id, {
+        $set: request.body,
+      });
+      if (!place) {
+        return response.status(404).json({ status: "Not Found" });
+      }
 
-    response.status(200).json({ status: `Place ${id} updated!` });
+      response.status(200).json({ status: `Place ${id} updated!` });
+    } catch (error) {
+      response.status(500).json({ status: "Internal Server Error" });
+    }
   }
+
   // DELETE request:
   if (request.method === "DELETE") {
-    await Place.findByIdAndDelete(id);
-    response.status(200).json({ status: `Place ${id} successfully deleted.` });
+    try {
+      await Place.findByIdAndDelete(id);
+      response
+        .status(200)
+        .json({ status: `Place ${id} successfully deleted.` });
+    } catch (error) {
+      response.status(500).json({ status: "Internal Server Error" });
+    }
   } else return response.status(405).json({ message: "Method not allowed" });
 }
